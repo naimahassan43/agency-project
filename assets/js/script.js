5a9de582bd2d041f026de3f2c838d68535119705
Syeda Naima Hassan <naima.hassan434@gmail.com>
2021-06-18 00:08:58 +0100

    add active class during scrolling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,50 @@
+// Sticky nav on scrolling
+const nav = document.querySelector("#menuBar");
+const NavTop = nav.offsetTop;
+let mainNavLinks = document.querySelectorAll("#navCont ul li a");
+let mainSections = document.querySelectorAll("section");
+
+
+function fixnavbar() {
+    if (window.scrollY >= NavTop) {
+        document.body.style.paddingTop = nav.offsetHeight + "px";
+        document.body.classList.add("fixed-nav");
+    } else {
+        document.body.style.paddingTop = 0;
+        document.body.classList.remove("fixed-nav");
+    }
+}
+
+function currentNavBar() {
+    let fromTop = window.scrollY;
+    const scrollPosition = document.documentElement.scrollTop;
+    mainSections.forEach(section => {
+
+        if (
+            scrollPosition >= section.offsetTop - section.offsetHeight * 0.25 &&
+            scrollPosition < section.offsetTop + section.offsetHeight - section.offsetHeight * 0.25
+        ) {
+            const currentId = section.attributes.id.value;
+            removeAllCurrentClasses();
+            addCurrentClass(currentId);
+        }
+    });
+}
+// remove current class function
+const removeAllCurrentClasses = function() {
+        mainNavLinks.forEach((el) => {
+            el.classList.remove('current');
+        });
+    }
+    // add current class function
+const addCurrentClass = function(id) {
+    console.log(id);
+    const selector = `nav a[href="#${id}"]`;
+    document.querySelector(selector).classList.add('current');
+}
+window.addEventListener("scroll", fixnavbar);
+window.addEventListener("scroll", currentNavBar);
+// Form 
 const dropForm = document.querySelector('#dropdownForm');
 document.querySelector('#btm-drop').addEventListener('click', function() {
     if (dropForm.style.display !== "none") {
